831b3f5ab9d900695341a2b00ee1ec146b20960f
Nikkolas <kolanvs@yandex.ru>
2020-05-23 22:13:48 +0300

    Initial commit

---
+++ src/com/kolanvs/feminist/Node.java
@@ -0,0 +1,19 @@
+package com.kolanvs.feminist;
+
+
+
+public class Node {
+
+    private Integer number;
+    private Double x;
+    private Double y;
+    private Double z;
+
+    private NodeList nodeList;
+
+    Node(Integer number, Double x, Double y, Double z, NodeList nodeList) {
+
+
+    }
+
+}
