991a7df423e6e6227817f887c42e88eff50fbcc8
Nikkolas <kolanvs@yandex.ru>
2020-05-23 23:11:50 +0300

    Add constructors for Node class and NodeList

--- src/com/kolanvs/feminist/Node.java
+++ src/com/kolanvs/feminist/Node.java
@@ -1,8 +1,16 @@
 package com.kolanvs.feminist;
 
 
+import java.util.ArrayList;
 
 public class Node {
+
+    /**
+     * The {@code Node} class describes a simple node in a finite element model.
+     * Contains the number and coordinates of the node.
+     *
+     * @author  Nikolay Boryaev
+     */
 
     private Integer number;
     private Double x;
@@ -11,7 +19,17 @@ public class Node {
 
     private NodeList nodeList;
 
-    Node(Integer number, Double x, Double y, Double z, NodeList nodeList) {
+    Node(Integer nodeNumber, NodeList nodeList) {
+
+        this.number = nodeNumber;
+        this.nodeList = nodeList;
+
+
+        ArrayList<Double> coords = this.nodeList.getCoords(this.number);
+
+        this.x = coords.get(0);
+        this.y = coords.get(1);
+        this.z = coords.get(2);
 
 
     }
