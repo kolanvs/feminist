e8f6484926c39365d95f8b9e14f78d3dd957e12e
Nikkolas <kolanvs@yandex.ru>
2020-05-23 23:14:04 +0300

    Add constructors for Node class and NodeList

--- src/com/kolanvs/feminist/NodeList.java
+++ src/com/kolanvs/feminist/NodeList.java
@@ -1,4 +1,35 @@
 package com.kolanvs.feminist;
 
+import java.util.*;
+
 public class NodeList {
+
+    private Map<Integer, ArrayList<Double>> nodeListMap;
+    private Integer size;
+
+
+    NodeList(ArrayList<Integer> numberArray, ArrayList<ArrayList<Double>> coordsArray){
+
+        this.size = numberArray.size();
+
+        try {
+            for (int i = 0; i < this.size; i++)
+                this.nodeListMap.put(numberArray.get(i), coordsArray.get(i));
+        }
+        catch (NullPointerException e) {
+            System.out.println("Error! Null pointer");
+        }
+
+
+    }
+
+
+    ArrayList<Double> getCoords(Integer nodeNumber){
+
+        return nodeListMap.get(nodeNumber);
+
+    }
+
+
+
 }
