d03995776d98272e8a5401ae29bfee9d247dc5ee
Nikkolas <kolanvs@yandex.ru>
2020-05-26 21:29:26 +0300

    Added external files of nodes and elements. Added class for reading node files.

--- src/com/kolanvs/feminist/Node.java
+++ src/com/kolanvs/feminist/Node.java
@@ -17,21 +17,33 @@ public class Node {
     private Double y;
     private Double z;
 
+    private ArrayList<Integer> connectedElems;
+    private Integer quantityConnectedElems = 0;
+
+
     private NodeList nodeList;
 
-    Node(Integer nodeNumber, NodeList nodeList) {
+    Node(Integer nodeNumber, ArrayList<Double> coords) {
 
         this.number = nodeNumber;
-        this.nodeList = nodeList;
-
-
-        ArrayList<Double> coords = this.nodeList.getCoords(this.number);
 
         this.x = coords.get(0);
         this.y = coords.get(1);
         this.z = coords.get(2);
 
-
     }
+
+    public ArrayList<Double> getCoords(){
+
+        ArrayList<Double> coords = new ArrayList<>(3);
+
+        coords.set(0,this.x);
+        coords.set(1,this.y);
+        coords.set(2,this.z);
+
+        return coords;
+    }
+
+
 
 }
